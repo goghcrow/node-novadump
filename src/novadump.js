36056f2b25c3e761ef60d8ff3828bc9cd1ce3387
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-24 02:49:38 +0800

    update

--- src/novadump.js
+++ src/novadump.js
@@ -23,51 +23,61 @@ tcpTracker.on("session", function (session) {
   let sendBuf = new MuduoBuffer()
   let recvBuf = new MuduoBuffer()
 
-  session.on("data send", function (session, data) {
-    // console.log(session.src_name + " -> " + session.dst_name + " data send " + session.send_bytes_payload + " + " + data.length + " bytes")
-    if (isNovaSession === void 0) {
-      sendBuf.append(data)
-      isNovaSession = nova.detect(sendBuf)
-    }
 
-    if (isNovaSession === void 0) {
-      return
-    } else if (isNovaSession === false) {
-      sendBuf = void 0
-      return
-    } else if (isNovaSession === true) {
-      sendBuf.append(data)
-      let msgSize = sendBuf.peekInt32()
-      if (sendBuf.readableBytes() >= msgSize) {
-        let novaBuf = sendBuf.read(msgSize)
-        let { ip, port, service, method, seq, attach, thriftBuffer } = nova.decode(novaBuf)
-        console.log(`send ${service}:${method}`)
+  session.on("data send", (function () {
+    let detected
+    return function (session, data) {
+      // console.log(session.src_name + " -> " + session.dst_name + " data send " + session.send_bytes_payload + " + " + data.length + " bytes")
+      if (detected === void 0) {
+        sendBuf.append(data)
+        detected = nova.detect(sendBuf)
+      }
+
+      if (detected === void 0) {
+        return
+      } else if (detected === false) {
+        sendBuf = void 0
+        return
+      } else if (detected === true) {
+        isNovaSession = true
+        sendBuf.append(data)
+        let msgSize = sendBuf.peekInt32()
+        if (sendBuf.readableBytes() >= msgSize) {
+          let novaBuf = sendBuf.read(msgSize)
+          let { ip, port, service, method, seq, attach, thriftBuffer } = nova.decode(novaBuf)
+          console.log(`send ${service}:${method}`)
+        }
       }
     }
-  })
+  }()))
 
-  session.on("data recv", function (session, data) {
-    // console.log(session.dst_name + " -> " + session.src_name + " data recv " + session.recv_bytes_payload + " + " + data.length + " bytes")    
-    if (isNovaSession === void 0) {
-      recvBuf.append(data)
-      isNovaSession = nova.detect(recvBuf)
-    }
 
-    if (isNovaSession === void 0) {
-      return
-    } else if (isNovaSession === false) {
-      recvBuf = void 0
-      return
-    } else if (isNovaSession === true) {
-      recvBuf.append(data)
-      let msgSize = recvBuf.peekInt32()
-      if (recvBuf.readableBytes() >= msgSize) {
-        let novaBuf = recvBuf.read(msgSize)
-        let { ip, port, service, method, seq, attach, thriftBuffer } = nova.decode(novaBuf)
-        console.log(`recv ${service}:${method}`)
+  session.on("data recv", (function () {
+    let detected
+    return function (session, data) {
+      // console.log(session.dst_name + " -> " + session.src_name + " data recv " + session.recv_bytes_payload + " + " + data.length + " bytes")    
+      if (detected === void 0) {
+        recvBuf.append(data)
+        detected = nova.detect(recvBuf)
+      }
+
+      if (detected === void 0) {
+        return
+      } else if (detected === false) {
+        recvBuf = void 0
+        return
+      } else if (detected === true) {
+        isNovaSession = true
+        recvBuf.append(data)
+        let msgSize = recvBuf.peekInt32()
+        if (recvBuf.readableBytes() >= msgSize) {
+          let novaBuf = recvBuf.read(msgSize)
+          let { ip, port, service, method, seq, attach, thriftBuffer } = nova.decode(novaBuf)
+          console.log(`recv ${service}:${method}`)
+        }
       }
     }
-  })
+  }()))
 
   session.on("end", function (session) {
     if (isNovaSession === true) {
